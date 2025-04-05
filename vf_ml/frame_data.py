9d607eb709747263b82d5e7c176e9429e42c0c38
SuperGolden <4214415+SuperGoldenZ@users.noreply.github.com>
2025-04-05 20:01:00 +0900

    moved to subclasses

--- vf_ml/frame_data.py
+++ vf_ml/frame_data.py
@@ -1,19 +1,12 @@
+from vf_ml import player_frame_data
 from dataclasses import dataclass
 
 @dataclass
 class FrameData:
     """Class for holding data of a frame in Virtua Fighter"""
     stage: str
-    p1_health: int
-    p2_health: int
-    p1_rank: int
-    p2_rank: int
-    p1_drinks: int
-    p2_drinks: int
-    p1_ringname: str
-    p2_ringname: str
-    p1_rounds_won_so_far: int
-    p2_rounds_won_so_far: int
     time_remaining: float
+    p1_frame_data: player_frame_data.PlayerFrameData
+    p2_frame_data: player_frame_data.PlayerFrameData
     
     
