3a1917c5e7ed9919538c95152f0de66fb5e71ade
SuperGolden <4214415+SuperGoldenZ@users.noreply.github.com>
2025-04-06 17:38:51 +0900

    demo for win probability

---
+++ demos/win_probability_chart_demo.py
@@ -0,0 +1,35 @@
+import os
+
+import vf_ml.data_helper
+import argparse
+
+parser = argparse.ArgumentParser(description="File with CSV data from a match")
+parser.add_argument("--filename", help="csv or gzip file", type=str, required=True)
+
+args = parser.parse_args()
+
+# Access the filename
+filename = args.filename
+
+if not os.path.exists(filename):
+    print(f"{filename} does not exist")
+    exit(1)
+
+data = vf_ml.DataHelper.load_data(filename)
+win_probability_chart = vf_ml.WinProbabilityChart()
+
+#win_probability_chart.generate_win_prob_chart_with_single_line(
+    #round_number=data["Player 2 Rank"].max(),
+    #stage=data["Stage"],
+    #frame_data=data,
+    #p1rank=data["Player 1 Rank"],
+    #p2rank=data["Player 2 Rank"],
+#)
+
+win_probability_chart.generate_match_win_prob_chart_with_single_line(
+    round_number=data["Player 2 Rank"].max(),
+    stage=data["Stage"].iloc[0],
+    frame_data=data,
+    p1rank=float(data["Player 1 Rank"].iloc[0]),
+    p2rank=float(data["Player 2 Rank"].iloc[0]),
+)
