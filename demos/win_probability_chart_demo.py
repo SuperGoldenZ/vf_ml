e9dc84ed7e4d7befe5b937c1420666f86926cfca
SuperGolden <4214415+SuperGoldenZ@users.noreply.github.com>
2025-04-07 20:46:06 +0900

    remove unnecessary parameters

--- demos/win_probability_chart_demo.py
+++ demos/win_probability_chart_demo.py
@@ -19,11 +19,7 @@ data = vf_ml.DataHelper.load_data(filename, prepare_for_prediction=True)
 win_probability_chart = vf_ml.WinProbabilityChart()
 
 win_probability_chart.generate_match_win_prob_chart_with_single_line(
-    round_number=data["Player 2 Rank"].max(),
-    stage=data["Stage"].iloc[0],
     frame_data=data,
-    p1rank=float(data["Player 1 Rank"].iloc[0]),
-    p2rank=float(data["Player 2 Rank"].iloc[0]),
     p1character=data["Player 1 Character"].iloc[0],
     p2character=data["Player 2 Character"].iloc[0]
 )
